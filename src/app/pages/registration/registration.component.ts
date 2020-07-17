f7f88aacdf6295a49d731171531d0474191e6a62
Kris <test@alkosafe.pl>
2020-07-17 20:29:58 +0200

    fix

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,13 +1,13 @@
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { checkPasswords } from 'app/helpers/validators/passwordValidator';
 import { checkEmail } from 'app/helpers/validators/asyncEmailValidator';
 
 @Component({
-  selector: "app-registration",
-  templateUrl: "./registration.component.html",
-  styleUrls: ["./registration.component.css"]
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
   constructor(public auth: AngularFireAuth) {
